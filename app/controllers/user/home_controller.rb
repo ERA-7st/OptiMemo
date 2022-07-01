72e9aa740417c754472a96e4836ae4f835336481
ERA-7st <are123230k@gmail.com>
2022-07-01 17:14:37 +0900

    add_home_top

---
+++ app/controllers/user/home_controller.rb
@@ -0,0 +1,7 @@
+class User::HomeController < ApplicationController
+
+  def top
+    
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # devisse_for :users
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: 'user/home#top'
   # Defines the root path route ("/")
   # root "articles#index"
 end
