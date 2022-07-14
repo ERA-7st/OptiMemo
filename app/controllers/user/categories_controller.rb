c510191698fe0a9977b7f194ef11dec684f8f394
ERA-7st <are123230k@gmail.com>
2022-07-14 16:34:28 +0900

    add_category_create

--- app/controllers/user/categories_controller.rb
+++ app/controllers/user/categories_controller.rb
@@ -3,7 +3,27 @@ class User::CategoriesController < ApplicationController
   before_action :user_logged_in?
 
   def index
-    @categories = current_user.categories.all
+    @categories = current_user.categories.all.by_recently_created
+    @new_category = Category.new
+  end
+
+  def create
+    category = current_user.categories.new(category_params)
+    if category.save
+      redirect_back(fallback_location: user_home_top_path)
+    else
+      render turbo_stream: turbo_stream.replace(
+        "errors",
+        partial: 'layouts/error_message',
+        locals: { model: category },
+      )
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   end
 
   scope module: :user do
-    resources :categories, only: [:index]
+    resources :categories, only: [:index, :create]
   end
 
 end
