dd57f3a29463483a8392ad700df9eba67acfe516
ERA-7st <are123230k@gmail.com>
2022-07-29 02:04:53 +0900

    add_check

--- app/controllers/user/reviews_controller.rb
+++ app/controllers/user/reviews_controller.rb
@@ -1,11 +1,37 @@
 class User::ReviewsController < ApplicationController
 
   before_action :user_logged_in?
+  before_action :set_words, only: [:index, :check]
+  before_action :word_id_certification, only: [:confirm]
   
-  def index
+  def indexd
+    @display = "false"
+  end
+
+  def check
+    @word = @words.first
+  end
+
+  def confirm
+    render turbo_stream: turbo_stream.replace(
+      "word_content",
+      partial: 'user/reviews/word_content',
+      locals: { display: "true", word: @word}
+    )
+  end
+
+  def set_words
     @words =  current_user.words
       .eager_load(:score)
       .where(score: {days_left: 0})
+      .order("score.updated_at ASC")
+  end
+
+  def word_id_certification
+    @word = current_user.words.find(params[:id])
+    unless @word || @word.days_left == 0
+      redirect_back(fallback_location: user_home_top_path)
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :create, :destroy]
     resources :words
     resources :reviews, only: [:index]
+    get "reviews/check" => "reviews#check", as: "check"
+    get "reviews/confirm/:id" => "reviews#confirm", as: "confirm"
     get "category_words/modal_index" => "category_words#modal_index", as: "category_words_modal_index"
     get "category_words/set_category/:id" => "category_words#set_category", as: "set_category"
     get "category_words/remove_category/:id" => "category_words#remove_category", as: "remove_category"
