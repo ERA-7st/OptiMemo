ccf1998f90899338fe20296b79695bcca3702c23
ERA-7st <are123230k@gmail.com>
2022-12-21 13:48:40 +0900

    add_review_count

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def unchecked_words
+    words_count = current_user.words
+                              .eager_load(:score)
+                              .where(score: {days_left: 0})
+                              .count
+    if words_count > 99
+      "99+"
+    else
+      "#{words_count}"
+    end
+  end
+
 end
