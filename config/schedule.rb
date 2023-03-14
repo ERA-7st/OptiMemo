a0eacd3c401c86f3a555430b83ff9dba1c021a13
ERA-7st <are123230k@gmail.com>
2023-03-14 23:33:31 +0900

    modify_cagories_index

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,6 @@ set :environment, :development
 
 set :output, "#{Rails.root}/log/cron.log"
 
-# every 1.minute do
 every 1.day, at: '00:00 am' do
   begin
     rake "days_left:update"
@@ -13,10 +12,12 @@ every 1.day, at: '00:00 am' do
   end
 end
 
+
 every 1.day, at: "12:00 pm" do
   begin
     rake "review_mail:send_email"
   rescue => e
     raise e
   end
+
 end
